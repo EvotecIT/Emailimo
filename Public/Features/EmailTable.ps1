5a51ce3f5ec36bb4d1799f19a72f780faccc2b97
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2019-04-13 19:06:15 +0200

    More EmailTable features

--- Public/Features/EmailTable.ps1
+++ Public/Features/EmailTable.ps1
@@ -1,7 +1,41 @@
 function EmailTable {
     [CmdletBinding()]
     param(
-        $Table
+        [Parameter(Mandatory = $false, Position = 0)][ScriptBlock] $ConditionalFormatting,
+        [alias('Table')][Array] $DataTable,
+        [string[]][ValidateSet('copyHtml5', 'excelHtml5', 'csvHtml5', 'pdfHtml5')] $Buttons = @('copyHtml5', 'excelHtml5', 'csvHtml5', 'pdfHtml5'),
+        [string[]][ValidateSet('numbers', 'simple', 'simple_numbers', 'full', 'full_numbers', 'first_last_numbers')] $PagingStyle = 'full_numbers',
+        [int[]]$PagingOptions = @(15, 25, 50, 100),
+        [switch]$DisablePaging,
+        [switch]$DisableOrdering,
+        [switch]$DisableInfo,
+        [switch]$HideFooter,
+        [switch]$DisableColumnReorder,
+        [switch]$DisableProcessing,
+        [switch]$DisableResponsiveTable,
+        [switch]$DisableSelect,
+        [switch]$DisableStateSave,
+        [switch]$DisableSearch,
+        [switch]$ScrollCollapse,
+        [switch]$OrderMulti,
+        [switch]$Filtering,
+        [ValidateSet('Top', 'Bottom', 'Both')][string]$FilteringLocation = 'Bottom',
+        [string[]][ValidateSet('display', 'cell-border', 'compact', 'hover', 'nowrap', 'order-column', 'row-border', 'stripe')] $Style = @('display', 'compact'),
+        [string]$TextWhenNoData = 'No data available.',
+        [int] $ScreenSizePercent = 0,
+        [string[]] $DefaultSortColumn,
+        [int[]] $DefaultSortIndex,
+        [ValidateSet('Ascending', 'Descending')][string] $DefaultSortOrder = 'Ascending',
+        [alias('Search')][string]$Find
     )
-    New-HTMLTable -DataTable $Table
+    New-HTMLTable -DataTable $DataTable `
+        -HideFooter:$HideFooter `
+        -Buttons $Buttons -PagingStyle $PagingStyle -PagingOptions $PagingOptions `
+        -DisablePaging:$DisablePaging -DisableOrdering:$DisableOrdering -DisableInfo:$DisableInfo -DisableColumnReorder:$DisableColumnReorder -DisableProcessing:$DisableProcessing `
+        -DisableResponsiveTable:$DisableResponsiveTable -DisableSelect:$DisableSelect -DisableStateSave:$DisableStateSave -DisableSearch:$DisableSearch -ScrollCollapse:$ScrollCollapse `
+        -Style $Style -TextWhenNoData:$TextWhenNoData -ScreenSizePercent $ScreenSizePercent `
+        -ConditionalFormatting $ConditionalFormatting `
+        -DefaultSortColumn $DefaultSortColumn -DefaultSortIndex $DefaultSortIndex -DefaultSortOrder $DefaultSortOrder `
+        -Find $Find -OrderMulti:$OrderMulti `
+        -Filtering:$Filtering -FilteringLocation $FilteringLocation
 }
